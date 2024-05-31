948b4b2f621cc50f874da00bae02916b50c625fe
DuyNgo <duy.ngo@digitalfortress.dev>
2024-05-31 10:47:48 +0700

    implement logout feature - progress

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
-import { LoginController, RegisterController } from '~/controllers/users.controllers'
-import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+import { LoginController, LogoutController, RegisterController } from '~/controllers/users.controllers'
+import { accessTokenValidator, loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRoute = Router()
 
 usersRoute.post('/login', loginValidator, wrapRequestHandler(LoginController))
 usersRoute.post('/register', registerValidator, wrapRequestHandler(RegisterController))
+usersRoute.post(
+  '/logout',
+  accessTokenValidator,
+  wrapRequestHandler((req, res) => {
+    res.json({ msg: 'logout successfully' })
+  })
+)
 
 export default usersRoute
